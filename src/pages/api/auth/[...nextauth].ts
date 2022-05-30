f690ee686915a381f499532496ee0bfe84f18de8
Allan Andrade <allanandrade.empreendimentos@gmail.com>
2022-05-30 16:41:05 -0300

    Finalizando o modulo de pagamento do stripe

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,52 @@
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+
+import { fauna } from '../../../services/fauna'
+import {query as q} from "faunadb";
+
+export default NextAuth({
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+            authorization: {
+                params: {
+                    scope: 'read:user'
+                }
+            },
+        }),
+    ],
+    callbacks: {
+        async signIn({ user, account, profile,  credentials }) {
+            const { email } = user
+
+            try {
+                await fauna.query(
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index('user_by_email'),
+                                    q.Casefold(email as string)
+                                )
+                            )
+                        ),
+                        q.Create(
+                            q.Collection('users'),
+                            { data: { email } }
+                        ),
+                        q.Get(
+                            q.Match(
+                                q.Index('user_by_email'),
+                                q.Casefold(email as string)
+                            )
+                        )
+                    )
+                )
+                return true
+            } catch {
+                return false
+            }
+        },
+    }
+})
